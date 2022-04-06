37df9bf5768cc7a5a457fc5b59bdd182d433eede
sergax <serey.aksenchenko@gmail.com>
2022-04-06 16:56:59 +0300

    add method toModel for getting single item resource

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.exception.EmployeeNotFoundException;
 import com.example.demo.model.Employee;
+import com.example.demo.model.EmployeeModelAssembler;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -21,8 +22,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 public class EmployeeController {
     private final EmployeeRepository repository;
 
-    public EmployeeController(EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    public EmployeeController(EmployeeRepository repository,
+                              EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     /**
@@ -30,22 +35,19 @@ public class EmployeeController {
      * includes not only the data but a collection of links.
      */
     @GetMapping("/employees/{id}")
-    EntityModel<Employee> one(@PathVariable Long id) {
+    public EntityModel<Employee> one(@PathVariable Long id) {
         Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @GetMapping("/employees")
-    CollectionModel<EntityModel<Employee>> all() {
+    public CollectionModel<EntityModel<Employee>> all() {
         List<EntityModel<Employee>> employees = repository.findAll().stream()
-                .map(employee -> EntityModel.of(employee,
-                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .map(assembler::toModel)
                 .collect(Collectors.toList());
+
         return CollectionModel.of(employees,
                 linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
---
+++ src/main/java/com/example/demo/model/EmployeeModelAssembler.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.example.demo.controller.EmployeeController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+/**
+ * by Aksenchenko Serhii on 06.04.2022
+ */
+
+@Component
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+}
